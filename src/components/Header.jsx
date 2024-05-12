d6282085b195e457b139a307b8c49a8794c96a21
Agung1606 <agngsptra928@gmail.com>
2024-05-12 14:48:59 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { neroErc } from "../assets";
+import { navigation, navigationSosmed } from "../constant";
+import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import { HamburgerMenu } from "./design/Header";
+import { useLocation } from "react-router-dom";
+
+const Header = () => {
+  const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+  return (
+    <div
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
+      <div className="flex items-center px-6 lg:px-7.5 xl:px-10 max-lg:py-4">
+        <div className="flex items-center gap-2">
+          <img src={neroErc} width={65} height={65} alt="logo-brand" />
+          <p className="font-sans font-semibold text-4xl">neroErc</p>
+        </div>
+
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-5 cursor-pointer px-6 py-6 md:py-8 lg:text-sm lg:text-n-2 lg:-mr-1 lg:font-semibold lg:hover:text-n-1 lg:leading-5`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+          <HamburgerMenu />
+        </nav>
+
+        <div className="hidden lg:flex items-center gap-2 ml-auto md:gap-4 lg:gap-6">
+          {navigationSosmed.map((item) => (
+            <a key={item.id}>
+              <img src={item.url} width={30} height={30} alt={item.title} />
+            </a>
+          ))}
+        </div>
+
+        <Button className={"hidden ml-[3rem] lg:flex"} href={"#launchapp"}>
+          Launch App
+        </Button>
+
+        <Button
+          className={"ml-auto lg:hidden"}
+          px={"px-3"}
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/constant/index.js
@@ -0,0 +1,60 @@
+import {
+  discord,
+  file02,
+  homeSmile,
+  notification2,
+  notification3,
+  notification4,
+  plusSquare,
+  searchMd,
+  telegram,
+  twitter,
+  yourlogo,
+} from "../assets";
+
+export const navigation = [
+  {
+    id: "1",
+    title: "Home",
+    url: "",
+  },
+  {
+    id: "2",
+    title: "$NRC",
+    url: "",
+  },
+  {
+    id: "3",
+    title: "Whitepaper",
+    url: "",
+  },
+  {
+    id: "4",
+    title: "Roadmap",
+    url: "",
+  },
+];
+
+export const navigationSosmed = [
+  {
+    id: "1",
+    url: telegram,
+    title: "telegram",
+  },
+  {
+    id: "2",
+    url: twitter,
+    title: "twitter",
+  },
+  {
+    id: "2",
+    url: discord,
+    title: "discord",
+  },
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo]
